65c1ea508d67df99ef83fa1ce068ac7260942709
Marcin Rim <marcin.rim@gmail.com>
2015-10-21 20:57:36 +0200

    FatekTarget refactoring

--- fatek/target.py
+++ fatek/target.py
@@ -1,17 +1,45 @@
 from symbol import Symbol
 
+
 class FatekTarget(object):
-    client, symbol = None, None
-    current_value = False # bool - current value - (coil/register access)
-    read, write = None, None # access functions
+    """
+        Manipulate on PLC objects
+
+        accessible read, write, read_all function,
+        which are picked according to desred Symbol
+    """
+
+    client = None  # Fatek instance
+    symbol = None  # Symbol
+
+    # TODO figure out whats that
+    current_value = False  # whether is coil or register (?)
+
+    # function handlers
+    read = None
+    write = None
 
     def __init__(self, client, symbol_str, current_value=False):
         self.client = client
         self.current_value = current_value
         self.symbol = Symbol(symbol_str)
-        self.__assign_functions()
 
-    def __assign_functions(self):
+        self._assign_functions()
+
+    def read_all(self, count):
+        """ Read all available stuff from PLC """
+        target = self.symbol.register
+        number = self.symbol.offset
+
+        if target in ['Y', 'X', 'M', 'S'] or (not self.current_value and target in ['T', 'C']):
+            # params: (start, number of readed bits)
+            return self.client.read_coils(number, count).bits
+
+        elif target in ['R', 'D'] or (self.current_value and target in ['T', 'C']):
+            # params: (start, number of readed bits)
+            return self.client.read_holding_registers(number, count).registers
+
+    def _assign_functions(self):
         target = self.symbol.register
 
         if target in ['Y', 'X', 'M', 'S']:
@@ -22,24 +50,15 @@ class FatekTarget(object):
             self.write = self._write_holding_r
 
     def _read_coil(self):
-        return self.client.read_coils(self.symbol.offset, 1).bits[0] # (start coil, number of readed bits)
+        # params: (start coil, number of readed bits)
+        return self.client.read_coils(self.symbol.offset, 1).bits[0]
 
     def _write_coil(self, value):
         return self.client.write_coil(self.symbol.offset, value)
 
     def _read_holding_r(self):
-        return self.client.read_holding_registers(self.symbol.offset, 1).registers[0] # (start , number of readed bytes)
+        # params: (start , number of readed bytes)
+        return self.client.read_holding_registers(self.symbol.offset, 1).registers[0]
 
     def _write_holding_r(self, value):
         return self.client.write_register(self.symbol.offset, value)
-
-    def read_all(self, count):
-        target = self.symbol.register
-        number = self.symbol.offset
-        current_value = self.current_value
-
-        if target in ['Y', 'X', 'M', 'S'] or (current_value == False and target in ['T', 'C']):
-            return self.client.read_coils(number, count).bits # (start coil, number of readed bits)
-
-        elif target in ['R', 'D'] or (current_value == True and target in ['T', 'C']):
-            return self.client.read_holding_registers(number, count).registers # (start, number of readed bits)
