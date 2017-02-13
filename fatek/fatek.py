ba450a9b852b1f7e21bb9d6eefdf75ea46f7af9e
Marcin Rim <marcin.rim@netrounds.com>
2017-02-13 09:53:44 +0100

    Fatek class refactoring (#4)

--- fatek/fatek.py
+++ fatek/fatek.py
@@ -6,7 +6,6 @@ class Fatek(object):
 
     def __init__(self, address, port=502, logger=None):
         if not logger:
-            import logging
             self.logger = logging.getLogger(__name__)
             self.logger.setLevel(logging.ERROR)
         else:
@@ -16,16 +15,16 @@ class Fatek(object):
         self.client = ModbusTcpClient(address, port)
 
     def read(self, symbol):
-        t = FatekTarget(self.client, symbol)
-        return t.read()
+        target = FatekTarget(self.client, symbol)
+        return target.read()
 
     def write(self, symbol, value=True):
-        t = FatekTarget(self.client, symbol)
-        return t.write(value)
+        target = FatekTarget(self.client, symbol)
+        return target.write(value)
 
     def bulk_read(self, symbol, count, current_value=False):
         """
             current_value is for reading numeric values from T and C registers
         """
-        t = FatekTarget(self.client, symbol, current_value)
-        return t.read_all(int(count))
+        target = FatekTarget(self.client, symbol, current_value)
+        return target.read_all(int(count))
